34faf850cd0b94ab40127f200e0d3ce9b0596385
김주엽 <kjyy08@naver.com>
2025-03-01 14:28:33 +0900

    [Ch00] 브랜치 예시 - 김주엽

---
+++ chapter00/kjyy08/before/Before.java
@@ -0,0 +1,15 @@
+package kjyy08.before;
+
+public class Before {
+    class Naver {
+        void fetchAPI(){};
+    }
+
+    class Kakao {
+        void retrieveAPI(){};
+    }
+
+    class Toss {
+        void getAPI(){};
+    }
+}
