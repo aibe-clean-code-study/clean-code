1c63d19181cfea115d84f6ff9817d12d666d2251
soowan <nick6767676767@gmail.com>
2025-03-04 11:50:58 +0900

    [Ch02] 의미 있는 이름 - 박수완

---
+++ chapter02:cake0420/after/Solution/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        int MAX_ARRAY_SIZE = 10001;
+        boolean[] isSelfNumber = new boolean[MAX_ARRAY_SIZE];
+        for (int INDEX = 1; INDEX <= MAX_ARRAY_SIZE - 1; INDEX++) {
+            int next = getNext(INDEX);
+            while (next <= MAX_ARRAY_SIZE && !isSelfNumber[next]) {
+                isSelfNumber[next] = true;
+                next = getNext(next);
+            }
+        }
+
+        for (int INDEX = 1; INDEX <= MAX_ARRAY_SIZE - 1; INDEX++) {
+            if (!isSelfNumber[INDEX]) {
+                System.out.println(INDEX);
+            }
+        }
+    }
+
+    public static int getNext(int number) {
+        int nextNumber = number;
+        while (number > 0) {
+            nextNumber += number % 10;
+            number /= 10;
+        }
+        return nextNumber;
+    }
+}
