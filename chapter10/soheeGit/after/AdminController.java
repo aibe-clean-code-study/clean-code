3e5b4226fe8093830ee83905f061d0f8301292fc
soheeGit <63wlsthgml@gmail.com>
2025-03-31 11:46:06 +0900

    [Ch10] 클래스 - 진소희

---
+++ chapter10/soheeGit/after/AdminController.java
@@ -0,0 +1,36 @@
+package org.example.aquareserve.controller;
+
+import org.example.aquareserve.model.dto.SportDTO;
+import org.example.aquareserve.model.repository.SportRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    final private SportRepository sportRepository;
+
+    public AdminController(SportRepository sportRepository) {
+        this.sportRepository = sportRepository;
+    }
+
+    @GetMapping("/sport")
+    String sport() {
+        return "sport";
+    }
+
+    @PostMapping("/sport")
+    String addSport(@ModelAttribute SportDTO sportDTO) throws Exception {
+        sportRepository.save(new SportDTO(
+
+                sportDTO.name(),
+                sportDTO.description()
+        ));
+        return "redirect:/";
+    }
+}
